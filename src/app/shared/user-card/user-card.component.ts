f6d5ee1f4bc96511b91a481da9aa4fa8bc2b24d3
unknown <andresucero241093@gmail.com>
2024-03-29 14:36:22 -0400

    fix(app): download doc fixed

--- src/app/shared/user-card/user-card.component.ts
+++ src/app/shared/user-card/user-card.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { TranslateModule } from '@ngx-translate/core';
+import {
+  LangChangeEvent,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-user-card',
@@ -10,7 +14,22 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
+  resumePath = '/assets/docs/english_resume.pdf';
+
+  constructor(private readonly _translateService: TranslateService) {}
+
+  ngOnInit(): void {
+    this._translateService.onLangChange.subscribe({
+      next: (lang: LangChangeEvent) => {
+        this.resumePath =
+          lang.lang === 'en'
+            ? '/assets/docs/english_resume.pdf'
+            : '/assets/docs/spanish_resume.pdf';
+      },
+    });
+  }
+
   goToLink(url: string): void {
     window.open(url, '_blank');
   }
